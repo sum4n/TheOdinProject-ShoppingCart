2c0bcb6270574a630fea52a99b8dff0b518039d2
Suman Das <sum4ndas@gmail.com>
2024-01-22 21:21:31 +0530

    Moving all states into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import HomePageBody from "./components/HomePageBody/HomePageBody";
-import ShopPage from "./components/ShopPage/ShopPage";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [itemsInCart, setItemsInCart] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products", { mode: "cors" })
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error("server error");
+        }
+        return res.json();
+      })
+      .then((json) => setData(json))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const addToCart = (e, data) => {
+    setItemsInCart([...itemsInCart, data[e.target.id - 1]]);
+    console.log(e);
+  };
+
   return (
     <>
-      <NavigationBar />
-      <HomePageBody />
-      {/* <ShopPage /> */}
+      <NavigationBar itemsInCart={itemsInCart.length} />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,61 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import ShopPage from "./components/ShopPage/ShopPage.jsx";
 import "./index.css";
 import HomePageBody from "./components/HomePageBody/HomePageBody.jsx";
 import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 import { useState, useEffect } from "react";
 
-const handleAddToCart = () => {
-  const [itemsInCart, setItemsInCart] = useState([]);
-  function addToCart(e, data) {
-    setItemsInCart([...itemsInCart, data[e.target.id - 1]]);
-    console.log(e);
-  }
-  // console.log(e);
-  return { itemsInCart, addToCart };
-};
-
-const fetchData = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products", { mode: "cors" })
-      .then((res) => {
-        if (res.status >= 400) {
-          throw new Error("server error");
-        }
-        return res.json();
-      })
-      .then((json) => setData(json))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
-  }, []);
-
-  console.log(data);
-
-  return { data, error, loading };
-};
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePageBody handleAddToCart={handleAddToCart} />,
+    element: <App />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "shopPage",
-    element: (
-      <ShopPage
-        fetchData={fetchData}
-        handleAddToCart={handleAddToCart}
-        // itemsInCart={itemsInCart}
-      />
-    ),
   },
 ]);
 
