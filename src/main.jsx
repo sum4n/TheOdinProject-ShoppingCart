4d586c4f49a61c14bcbfffb75c41d8d2bc7d4789
Suman Das <sum4ndas@gmail.com>
2024-02-15 09:19:47 +0530

    Add 'dynamic segments' in router and remove 'HomePageBody' and 'ShopPage' imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import ShopPage from "./components/ShopPage/ShopPage.jsx";
 import "./index.css";
-import HomePageBody from "./components/HomePageBody/HomePageBody.jsx";
 import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/:name",
+    element: <App />,
     errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <HomePageBody /> }, // HomePageBody will be rendered by default
-      { path: "shopPage", element: <ShopPage /> },
-    ],
   },
 ]);
 
