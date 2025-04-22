7ade02e537a1558595bfc36d4c17151115a2c991
Suman Das <sum4ndas@gmail.com>
2025-04-22 19:26:11 +0530

    Refactor router: remove dynamic routes, add then as children.

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
+import ProductsPage from "./components/ProductsPage/ProductsPage.jsx";
+import HomePage from "./components/HomePage/HomePage.jsx";
+import ProductDetail from "./components/ProductDetail/ProductDetail.jsx";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/:name",
-    element: <App />,
-    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetail /> },
+      { path: "cart", element: <ShoppingCart /> },
+    ],
   },
 ]);
 
