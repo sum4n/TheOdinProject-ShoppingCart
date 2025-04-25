8e20d73bd973f1fb719c9d2cf76ab7f430556a47
Suman Das <sum4ndas@gmail.com>
2025-04-25 20:15:52 +0530

    Refactor and fix HomePage tests

--- src/components/HomePage/HomePage.test.jsx
+++ src/components/HomePage/HomePage.test.jsx
@@ -1,16 +1,33 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect } from "vitest";
-import HomePageBody from "./HomePageBody";
+import { beforeEach, describe, expect, it } from "vitest";
+import HomePage from "./HomePage";
 import { MemoryRouter } from "react-router-dom";
 
-describe("HomePageBody component", () => {
-  it("renders with prop totalItemsInCart with value 5", () => {
-    const { container } = render(
-      <MemoryRouter>
-        <HomePageBody totalItemsInCart={5} />
-      </MemoryRouter>
-    );
+vi.mock("./shopping_cart.webp", () => ({
+  default: "test_image_path.webp",
+}));
 
-    expect(container).toMatchSnapshot();
+describe("HomePage component", () => {
+  beforeEach(() => {
+    render(<HomePage />, { wrapper: MemoryRouter });
+  });
+
+  it("displays the welcome message", () => {
+    expect(
+      screen.getByText("Welcome to Odin Project Shopping Cart")
+    ).toBeInTheDocument();
+  });
+
+  it("has 'Shop' link with correct href", () => {
+    const shopLink = screen.getByRole("link", { name: /shop/i });
+    expect(shopLink).toBeInTheDocument();
+    expect(shopLink).toHaveAttribute("href", "/products");
+  });
+
+  it("has shopping cart image with proper attributes", () => {
+    const image = screen.getByRole("img", { name: /a shopping cart/i });
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", "test_image_path.webp");
+    expect(image).toHaveAttribute("alt", "a shopping cart");
   });
 });
