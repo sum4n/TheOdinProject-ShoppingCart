2266db929f00860630fb1b4684e9bde4c7d64875
Suman Das <sum4ndas@gmail.com>
2025-04-26 18:44:48 +0530

    Refactor NavigationBar tests.

--- src/components/NavigationBar/NavigationBar.test.jsx
+++ src/components/NavigationBar/NavigationBar.test.jsx
@@ -4,27 +4,36 @@ import NavigationBar from "./NavigationBar";
 import { MemoryRouter } from "react-router-dom";
 
 describe("NavigationBar component", () => {
-  it("renders correctly", () => {
+  it("renders with  5 links", () => {
     render(
       <MemoryRouter>
         <NavigationBar totalItemsInCart={1} />
       </MemoryRouter>
     );
-    expect(screen.getByText(/the shopping project/i)).toBeInTheDocument();
-    expect(screen.getByText(/cart: 1/i)).toBeInTheDocument();
-    expect(screen.getAllByRole("link")).length(3);
+
+    expect(screen.getAllByRole("link")).toHaveLength(5);
+    expect(
+      screen.getByRole("link", { name: /the shopping project/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /home/i })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /products/i })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /contact/i })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /cart/i })).toBeInTheDocument();
   });
 
-  // Avoid naming "renders correctly" snapshots.
-  // Create name that describes the purpose of the snapshot.
-  it("renders with 10 cart items", () => {
+  it("displays 8 cart items", () => {
+    render(<NavigationBar totalItemsInCart={8} />, { wrapper: MemoryRouter });
+
+    expect(screen.getByRole("link", { name: /cart: 8/i })).toBeInTheDocument();
+  });
+
+  it("matches snapshot with 10 items in the cart", () => {
     const { container } = render(
       <MemoryRouter>
         <NavigationBar totalItemsInCart={10} />
       </MemoryRouter>
     );
 
-    // snapshot test
     expect(container).toMatchSnapshot();
   });
 });
