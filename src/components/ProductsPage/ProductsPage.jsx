20f06c6f689803b4c22264b47ca6036cfd74acda
Suman Das <sum4ndas@gmail.com>
2025-04-28 11:17:48 +0530

    Refactor ProductsPage: transfer Product into its seperate component named ProductCard.

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -1,8 +1,9 @@
-import { Link, useOutletContext } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 import styles from "./ProductsPage.module.css";
+import ProductCard from "../ProductCard/ProductCard";
 
 const ProductsPage = () => {
-  const { data, error, loading, itemsInCart, addToCart } = useOutletContext();
+  const { data, error, loading } = useOutletContext();
 
   return (
     <>
@@ -15,70 +16,20 @@ const ProductsPage = () => {
             {data &&
               data.map((item) => {
                 return (
-                  <Product
+                  <ProductCard
                     key={item.id}
                     index={item.id}
                     title={item.title}
-                    description={item.description}
                     rating={item.rating}
                     price={item.price}
                     imgSource={item.image}
-                    handleAddToCart={(e) => addToCart(e, data)}
                   />
                 );
               })}
           </div>
-          {/* <ShoppingCart cartItems={itemsInCart} /> */}
         </div>
       </div>
     </>
-  );
-};
-
-const Product = ({
-  title,
-  index,
-  description,
-  rating,
-  price,
-  imgSource,
-  handleAddToCart,
-}) => {
-  return (
-    <Link to={`/products/${index}`}>
-      <div className={styles.productStyle}>
-        <img
-          className={styles.productImg}
-          src={imgSource}
-          alt="This is shop item"
-        />
-        <div className={styles.productText}>
-          <p className={styles.productTitle}>{title}</p>
-          {/* <p>{description}</p> */}
-          <p>
-            {rating.rate} of {rating.count}
-          </p>
-          <p className={styles.priceText}>Price: {price}$</p>
-          {/* <div>
-          <label className={styles.inputLabel} htmlFor="quantity">
-            Quantity
-          </label>
-          <input
-            className={styles.inputBox}
-            type="number"
-            id="quantity"
-            name="quantity"
-            max={10}
-            min={1}
-            defaultValue={1}
-          />
-        </div>
-        <button id={index} onClick={handleAddToCart}>
-          Add to cart
-        </button> */}
-        </div>
-      </div>
-    </Link>
   );
 };
 
