6d743ad8621032a821bd35a2ea2bfcf7578be92e
Suman Das <sum4ndas@gmail.com>
2025-04-28 11:19:38 +0530

    Create ProductCard to be used in Products and style it.

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import styles from "./ProductCard.module.css";
+
+const ProductCard = ({ title, index, rating, price, imgSource }) => {
+  return (
+    <Link to={`/products/${index}`}>
+      <div className={styles.productStyle}>
+        <img
+          className={styles.productImg}
+          src={imgSource}
+          alt="This is shop item"
+        />
+        <div className={styles.productText}>
+          <p className={styles.productTitle}>{title}</p>
+          <p>
+            {rating.rate} of {rating.count}
+          </p>
+          <p className={styles.priceText}>Price: {price}$</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
